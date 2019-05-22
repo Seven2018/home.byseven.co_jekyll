128e7275e8a21f45fc6f082e7b4a99ae1d92a997
Brice Chapuis <brice.chapuis@gmail.com>
2019-05-22 14:48:27 +0200

    minor fixes + added carousel modal

---
+++ _site/assets/javascripts/homeseven.js
@@ -0,0 +1,14 @@
+const initUpdateNavbarOnScroll = () => {
+  const navbar = document.querySelector('.navbar-seven');
+  if (navbar) {
+    window.addEventListener('scroll', () => {
+      if (window.scrollY >= window.innerHeight*.95) {
+        navbar.classList.add('scrolled');
+      } else {
+        navbar.classList.remove('scrolled');
+      }
+    });
+  }
+}
+
+initUpdateNavbarOnScroll();
---
+++ index.md
@@ -0,0 +1,8 @@
+---
+layout: home
+type: index
+title: "Learn by Seven"
+lang: fr
+---
+
+{{ content }}
