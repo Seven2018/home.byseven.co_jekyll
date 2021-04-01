bc2c950dd53366504eecf8785006fce99dc722d9
Marie Leleu <marieleleu@icloud.com>
2021-04-01 18:53:57 +0200

    odifications made on front

--- assets/javascripts/homeseven.js
+++ assets/javascripts/homeseven.js
@@ -2,7 +2,7 @@ const initUpdateNavbarOnScroll = () => {
   const navbar = document.querySelector('.navbar-seven');
   if (navbar) {
     window.addEventListener('scroll', () => {
-      if (window.scrollY >= window.innerHeight*.90) {
+      if (window.scrollY >= window.innerHeight*.82) {
         navbar.classList.add('scrolled');
       } else {
         navbar.classList.remove('scrolled');
